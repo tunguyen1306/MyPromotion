33ba0a47f09a276a6088680b1ace0061c65954fc
Nguyen Tan Tu <Administrator@tantu.ringier.com.vn>
2016-08-19 17:54:08 +0700

    no message

--- app/src/main/java/com/example/user/navigationdrawer/LoginFragment.java
+++ app/src/main/java/com/example/user/navigationdrawer/LoginFragment.java
@@ -15,10 +15,15 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by user on 12/31/15.
@@ -28,6 +33,7 @@ public class LoginFragment extends Fragment{
     TextView txtShow;
     View view;
     private CallbackManager callbackManager;
+    LoginButton loginButton;
     private FacebookCallback<LoginResult> loginResultFacebookCallback=new FacebookCallback<LoginResult>() {
         @Override
         public void onSuccess(LoginResult loginResult) {
@@ -38,7 +44,34 @@ public class LoginFragment extends Fragment{
                 txtShow=(TextView)view.findViewById(R.id.txtShow);
                 txtShow.setText(profile.getName());
             }
+            String userId = loginResult.getAccessToken().getUserId();
+            GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(),
+                    new GraphRequest.GraphJSONObjectCallback(){
+                        @Override
+                        public void onCompleted(JSONObject object, GraphResponse response){
+                            try
+                            {
+                                String id = object.getString("id");
+                             String   Username = object.getString("name");
+                                String   Email = object.getString("email");
+                                   String gender = object.getString("gender");
+ //                                  String birthday = object.getString("birthday");
+//                                    profileImgUrl = "https://graph.facebook.com/" + id + "/picture?type=large";
+//                                    getTokenKey(id);
+                                txtShow=(TextView)view.findViewById(R.id.txtShow);
+                                txtShow.setText(Username);
+                            }
+                            catch (JSONException e)
+                            {
+                                e.printStackTrace();
+                            }
 
+                        }
+                    });
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "id,name,gender,email,birthday");
+            request.setParameters(parameters);
+            request.executeAsync();
 
         }
 
@@ -53,27 +86,35 @@ public class LoginFragment extends Fragment{
         }
     };
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
-        callbackManager=CallbackManager.Factory.create();
-
-    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+//        callbackManager=CallbackManager.Factory.create();
+//        loginButton.setReadPermissions("user_friends");
+//        loginButton.setReadPermissions("email");
+//    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.nav_login,container,false);
+         loginButton =(LoginButton)view.findViewById(R.id.login_button);
+        loginButton.setFragment(this);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        callbackManager=CallbackManager.Factory.create();
+        loginButton.setReadPermissions("user_friends");
+        loginButton.setReadPermissions("email");
 
         return view;
     }
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        LoginButton loginButton =(LoginButton)view.findViewById(R.id.login_button);
+        loginButton =(LoginButton)view.findViewById(R.id.login_button);
         loginButton.setFragment(this);
         loginButton.registerCallback(callbackManager,loginResultFacebookCallback);
+
     }
 
     @Override
