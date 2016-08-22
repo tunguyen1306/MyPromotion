13de2fae1dfa0281ee94e8a1bb1e0c16a94f9f51
Nguyen Tan Tu <Administrator@tantu.ringier.com.vn>
2016-08-22 19:58:45 +0700

    no message

---
+++ app/src/main/java/com/example/user/navigationdrawer/ResClient.java
@@ -0,0 +1,12 @@
+package com.example.user.navigationdrawer;
+
+/**
+ * Created by Administrator on 8/22/2016.
+ */
+import retrofit.client.OkClient;
+import retrofit.converter.GsonConverter;
+
+public class ResClient {
+    public static final String UrlApi="";
+    private retrofit.
+}
